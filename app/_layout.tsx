c88258c063aab46f40930ec85ae1574dd33a9190
cherray <navarrocherry17@gmail.com>
2026-09-18 18:53:46 +0800

    Configure Expo Router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
+// Root layout component for the Expo Router
 import { Stack } from "expo-router";
+import { PlaceProvider } from "../context/PlaceContext";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <PlaceProvider>
+      <Stack />
+    </PlaceProvider>
+  );
 }
